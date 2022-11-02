abbe6fea437d13c6fba6bdfabd6ca090f7c464d5
esoterikosQ <seunggyubak@knou.ac.kr>
2022-11-02 10:45:44 +0000

    2022. 11. 2.

--- GitHub.r
+++ GitHub.r
@@ -33,6 +33,7 @@ credentials::set_github_pat("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")
 # 20220902 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
 # 20221006 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
 # 20221016 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
+# 20221102 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
 
 ## or store it manually in '.Renviron':
 usethis::edit_r_environ()
